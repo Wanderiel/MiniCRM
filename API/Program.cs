85f021ca98a5d0fee9c24e5042ecfe609a20e5db
Pavel Dymov <dpavel82@yandex.ru>
2026-05-07 21:19:06 +0300

    add Swagger

--- API/Program.cs
+++ API/Program.cs
@@ -5,8 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseSwaggerUI();
+app.UseSwagger();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
