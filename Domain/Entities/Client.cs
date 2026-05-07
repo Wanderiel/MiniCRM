8e40d5de19e9ca6b040348878c2362fb6b212ad2
Pavel Dymov <dpavel82@yandex.ru>
2026-05-07 20:12:45 +0300

    Сущности пользователя, клиента, сделка (пустоая)

---
+++ Domain/Entities/Client.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class Client
+    {
+        public Client(string name, string description, string email, int id)
+        {
+            Name = name;
+            Description = description;
+            Email = email;
+            Id = id;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Email { get; private set; }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public User(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+    }
+}
