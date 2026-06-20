102e3f9a1d72a55df7f5fd514d6e4cd726605bc4
Pavel Dymov <dpavel82@yandex.ru>
2026-06-20 15:16:01 +0300

    модель FullName

---
+++ Domain/Models/FullName.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.Models
+{
+    public class FullName
+    {
+        private readonly string _firstName;
+        private readonly string _lastName;
+
+        private FullName(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+
+        public static FullName Create(string firstName, string lastName)
+        {
+            if (string.IsNullOrWhiteSpace(firstName))
+                throw new ArgumentNullException($"Не верный формат {nameof(firstName)}");
+
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentNullException($"Не верный формат {nameof(lastName)}");
+
+            return new FullName(firstName, lastName);
+        }
+    }
+}
