b861ee7e06ed0c66be2d1f4ef1f78ca561d739fa
Pavel Dymov <dpavel82@yandex.ru>
2026-06-20 19:51:14 +0300

    Простенький PasswordHash

---
+++ Domain/Models/Exceptions/InvalidPasswordException.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Models.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException(string? reason = null)
+            : base(BuildMessage(reason))
+        { }
+
+        private static string BuildMessage(string? reason) => 
+            reason is null
+                ? $"Invalid password."
+                : $"Invalid password: {reason}.";
+    }
+}
---
+++ Domain/Models/Users/PasswordHash.cs
@@ -0,0 +1,35 @@
+﻿using Domain.HashGenerators;
+using Domain.Models.Exceptions;
+
+namespace Domain.Models.Users
+{
+    public class PasswordHash
+    {
+        private static int s_minLength = 8;
+        private string _passwordHash;
+
+        private PasswordHash(string passwordHash) => _passwordHash = passwordHash;
+
+        public string Value => _passwordHash;
+
+        public static PasswordHash Create(string password, string passwordRepeat)
+        {
+            if (password == passwordRepeat == false)
+                throw new InvalidPasswordException("Пароли не совпадают");
+
+            Validate(password);
+            string passwordHash = SHA256HashGenerator.Compute(password);
+
+            return new PasswordHash(passwordHash);
+        }
+
+        private static void Validate(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                throw new InvalidPasswordException($"Пароль не должен быть пустым");
+
+            if (password.Length < s_minLength)
+                throw new InvalidPasswordException($"Пароль должен быть не менее {s_minLength} символов");
+        }
+    }
+}
