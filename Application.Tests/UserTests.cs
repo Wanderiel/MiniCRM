bdcd0ac1158bd6a02f872886156aaed1921bb7d0
Pavel Dymov <dpavel82@yandex.ru>
2026-07-07 17:38:48 +0300

    UserTests

---
+++ Application.Tests/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Domain.Models.Users;
+using Moq;
+
+namespace Application.Tests;
+
+public class UserTests
+{
+    [Fact]
+    public void Register_Should_RegisterUser_When_EmailIsFree()
+    {
+        var repository = new Mock<IUsersRepository>();
+        var unitOfWork = new Mock<IUnitOfWork>();
+        var passwordHasher = new Mock<IPasswordHasher>();
+
+        repository.Setup(x => x.HasUserByEmailAsync(It.IsAny<Email>()))
+            .ReturnsAsync(false);
+
+        passwordHasher.Setup(x => x.Hash(It.IsAny<string>()))
+            .Returns("hash");
+    }
+}
