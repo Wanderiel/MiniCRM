c83fb12b06ca43dc0743fef7fb7d179ecc95ac8a
Pavel Dymov <dpavel82@yandex.ru>
2026-08-08 17:24:48 +0300

    Profiles

--- API/Program.cs
+++ API/Program.cs
@@ -5,13 +5,16 @@ using Infrastructure.Repositories;
 using Infrastructure.Security;
 using Microsoft.EntityFrameworkCore;
 
+const string PROFILE_LOCAL = "local";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+if (builder.Environment.EnvironmentName == PROFILE_LOCAL)
+    builder.Configuration.AddUserSecrets<Program>();
 
-builder.Configuration.AddUserSecrets<Program>();
 builder.Services.AddDbContext<IUnitOfWork, PostgresContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
@@ -25,7 +28,7 @@ builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == PROFILE_LOCAL)
 {
     app.MapOpenApi();
     app.UseSwaggerUI();
